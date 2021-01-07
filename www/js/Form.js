bf5015b1adea0d74733a100f78d82b4cb42af1c3
MaryemBenMohamed <maryoumabac18@gmail.com>
2021-01-08 00:30:14 +0100

    commit final

--- www/js/Form.js
+++ www/js/Form.js
@@ -1,39 +1,41 @@
+var firebaseConfig = {
+   apiKey: "AIzaSyDEpDavxrIgnhWoLMnSASW_gafHIcIZNPQ",
+   authDomain: "crossplatform-182d0.firebaseapp.com",
+   projectId: "crossplatform-182d0",
+   storageBucket: "crossplatform-182d0.appspot.com",
+   messagingSenderId: "126973651703",
+   appId: "1:126973651703:web:75c94220ddcea9a15b01f2",
+   measurementId: "G-6DN0Z3BKXE"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+ firebase.analytics();
+ const auth = firebase.auth();
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyDEpDavxrIgnhWoLMnSASW_gafHIcIZNPQ",
-    authDomain: "crossplatform-182d0.firebaseapp.com",
-    projectId: "crossplatform-182d0",
-    storageBucket: "crossplatform-182d0.appspot.com",
-    messagingSenderId: "126973651703",
-    appId: "1:126973651703:web:75c94220ddcea9a15b01f2",
-    measurementId: "G-6DN0Z3BKXE"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  const auth = firebase.auth();
+ function signUp(){
 
-  function signUp(){
-
-  		var email = document.getElementById("userEmail");
-  		var password = document.getElementById("userPassword");
-
-  		const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-  		promise.catch(e => alert(e.message));
-
-  		alert("You are now a member ,Go and sign in"+ email.value);
-
-  	}
+      var email = document.getElementById("userEmail");
+      var password = document.getElementById("userPassword");
+      if(email.value !="" ){
+           if (password.value!=""){
+           const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+           alert("You are now a member ,Go and sign in  "+ email.value);
 
 
-	function signIn(){
 
-		var email = document.getElementById("Email");
-		var password = document.getElementById("Password");
-		const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-		promise.catch(e => alert(e.message));
-		window.open("Home.html");
-
-	}
+           }
+      }
+      else{
+      alert("mistake"+ email.value);
+      }
+   }
 
 
+function signIn(){
+
+          var email = document.getElementById("Email");
+          var password = document.getElementById("Password");
+          const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+          promise.catch(e => alert(e.message));
+          promise.then (() => location.replace('Home.html'));
+       }
